979abc9411f586ca1b9cd5b01c2732a9c2d4b155
mgrkan <79905545+mgrkan@users.noreply.github.com>
2022-12-11 19:13:40 +0300

    inital commit

---
+++ wapp_to_json.py
@@ -0,0 +1,33 @@
+import json
+import re
+
+def wapp_to_json(chat_location)
+
+    pattern = "([1-9]|1[0-2])/([1-9]|[1-2][0-9]|3[0-1])/([1-9][1-9]), ([0-1][0-9]|2[0-3]):([0-5][0-9]) - "
+
+    with open(chat_location, "r", encoding = "utf-8") as f:
+        chat = f.readlines()
+
+    chat_json = []
+    for i in chat:
+        msg_arr = re.split(pattern, i)
+        msg_arr.pop(0)
+        
+        try:
+            msg = msg_arr[5]
+        except: continue
+        
+        b = ""
+        for a in msg:
+            if(a == ":"):
+                break
+            a = b + a
+            b = a
+        msg = msg.replace((b + ": "), "")
+        msg_dict = { "Sender": b, "Day": msg_arr[1], "Month": msg_arr[0], 
+        "Year": msg_arr[2], "Hour": msg_arr[3], "Message": msg  }
+        
+        chat_json.append(msg_dict)
+
+    chat_json = json.dumps(chat_json, ensure_ascii=False)
+    return chat_json
